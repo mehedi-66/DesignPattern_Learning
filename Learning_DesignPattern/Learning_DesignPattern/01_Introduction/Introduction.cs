7b283b1873e752d31362e4b6e34db3cdeb2b9aad
Mehedi Hasan <19303001@iubat.edu>
2023-09-15 19:30:29 +0600

    Start Learning Design Pattern

---
+++ Learning_DesignPattern/Learning_DesignPattern/01_Introduction/Introduction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_DesignPattern._01_Introduction
+{
+    public class Introduction
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Introduction is start Here");
+        }
+    }
+}
---
+++ Learning_DesignPattern/Learning_DesignPattern/Program.cs
@@ -0,0 +1,7 @@
+﻿class DesignPatter
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Start Learning Design Pattern");
+    }
+}
